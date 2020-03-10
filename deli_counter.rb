295ab7d78f8efa9a487862b90c881eb96fabdf17
YUKIKO IKEDA <applelele@gmail.com>
2020-03-10 23:27:33 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -29,7 +29,7 @@ end
 
 def now_serving(katz_deli)
   
-  if katz_deli < 1
+  if katz_deli.length < 1
     puts "There is nobody waiting to be served!"
   else
     katz_deli.each do |next_customer|
