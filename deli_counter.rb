cd1640383d0e1661c22cfedc7fc792e20423430c
YUKIKO IKEDA <applelele@gmail.com>
2020-03-10 23:01:45 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,11 +1,15 @@
+require "pry"
+
 def line(katz_deli)
     if katz_deli.length < 1
       puts "The line is currently empty."
     else
-      puts "The line is currently:"
+      message = "The line is currently:"
       katz_deli.each_with_index do |name, index|
-        print " #{index+1}. #{name}"
+        message += " #{index+1}. #{name}"
       end
+      binding.pry
+      message
     end
 end
 
